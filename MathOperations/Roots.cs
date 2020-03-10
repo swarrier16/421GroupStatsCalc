121e36828febe5dc1f4bcce95ee5d37fc15c6b54
Shruti Warrier <swarrier@shrutis-mbp.resource.campus.njit.edu>
2020-03-10 10:40:55 -0400

    added square root functionality

---
+++ MathOperations/Roots.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathOperations
+{
+    public static class Roots
+    {
+        static public double Root(double radicand)
+        {
+            var root = Math.Sqrt(radicand);
+
+            return root;
+        }
+    }
+}
--- basicCalculator/basicCalculator.cs
+++ basicCalculator/basicCalculator.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using MathOperations;
+using Multiplication;
+using SquareRoot;
 
 namespace basicCalculator
 {
-    public class basicCalculator
+    public class basicCalculator: IMultiply, IRoot
     {
 
         private static dynamic _result;
@@ -14,18 +16,21 @@ namespace basicCalculator
             set => _result = value;
         }
 
-        //Multiplying Multiplication = new Multiplying();
-
-        public static void Main()
-        {
-        }
+        Multiplying Multiplication = new Multiplying();
+        SquareRooting SquareRoot = new SquareRooting();
 
         public dynamic Product(dynamic a, dynamic b)
         {
             Result = Multiplication.Product(a, b); ;
             return Result;
         }
-     
+
+        public dynamic Root(dynamic a)
+        {
+            Result = Roots.Root(a);
+            return Result;
+        }
+
     }
 }
 
