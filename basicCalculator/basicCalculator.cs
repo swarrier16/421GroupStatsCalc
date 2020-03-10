bcc75e4d46dae143ca90b7c4629c599fdad8983a
Shruti Warrier <swarrier@shrutis-mbp.resource.campus.njit.edu>
2020-03-10 10:22:16 -0400

    added base multiplication functions

--- basicCalculator/basicCalculator.cs
+++ basicCalculator/basicCalculator.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using MathOperations;
 
 namespace basicCalculator
 {
-    public class Calculator
+    public class basicCalculator
     {
+
+        private static dynamic _result;
+
+        public static dynamic Result
+        {
+            get => _result;
+            set => _result = value;
+        }
+
+        //Multiplying Multiplication = new Multiplying();
+
+        public static void Main()
+        {
+        }
+
+        public dynamic Product(dynamic a, dynamic b)
+        {
+            Result = Multiplication.Product(a, b); ;
+            return Result;
+        }
+     
     }
 }
+
