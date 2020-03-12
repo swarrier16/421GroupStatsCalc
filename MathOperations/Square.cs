72fa1539b8ab8cfdff0f1e6e1eb6ab3b83aedd82
Rati <rjp76@njit.edu>
2020-03-11 23:08:43 -0400

    Subtraction and Square Added

---
+++ MathOperations/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathOperations
+{
+    public static class Squares
+    {
+        static public int Square(int squareBase)
+        {
+            var product = squareBase * squareBase;
+
+            return product;
+        }
+        static public double Square(double squareBase)
+        {
+            var product = squareBase * squareBase;
+
+            return product;
+        }
+    }
+}
---
+++ Square/Squaring.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MathOperations;
+using Microsoft.CSharp;
+
+namespace Square
+{
+    public interface ISquare
+    {
+        dynamic Square(dynamic a);
+    }
+    public class Square : ISquare
+    {
+        private static dynamic _result;
+
+        public static dynamic Result
+        {
+            get => _result;
+            set => _result = value;
+        }
+        public static void Main()
+        {
+        }
+        public dynamic Difference(dynamic a)
+        {
+            Result = MathOperations.Squares.Square(a); ;
+            return Result;
+        }
+
+    }
+}
