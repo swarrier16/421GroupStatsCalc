4d31f9306144d15b4d95bdc3e0f3c9e6b6788e5a
Shruti Warrier <swarrier@Shrutis-MBP.fios-router.home>
2020-03-13 01:18:37 -0400

    added mode

---
+++ Square/Squaring.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MathOperations;
+using Microsoft.CSharp;
+
+namespace Square
+{
+    public interface ISquare
+    {
+        dynamic SquareExpo(dynamic a);
+    }
+    public class Squaring : ISquare
+    {
+        private static dynamic _result;
+        public static dynamic Result
+        {
+            get => _result;
+            set => _result = value;
+        }
+        public static void Main()
+        {
+        }
+        public dynamic SquareExpo(dynamic a)
+        {
+            Result = MathOperations.Square.SquareMethod(a); ;
+            return Result;
+        }
+
+    }
+}
