bb350242f9dcf03812844367d67c2360d0fa0835
Shruti Warrier <swarrier@Shrutis-MBP.fios-router.home>
2020-03-13 01:02:28 -0400

    pulled square and subtraction

---
+++ Squaring/Squaring.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MathOperations;
+using Microsoft.CSharp;
+
+namespace Square
+{
+    public interface ISquare
+    {
+        dynamic SquareExpo(dynamic a);
+    }
+    public class Squaring : ISquare
+    {
+        private static dynamic _result;
+        public static dynamic Result
+        {
+            get => _result;
+            set => _result = value;
+        }
+
+        public dynamic SquareExpo(dynamic a)
+        {
+            Result = MathOperations.Square.SquareMethod(a); ;
+            return Result;
+        }
+
+    }
+}
---
+++ Subtraction/Subtracting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MathOperations;
+using Microsoft.CSharp;
+
+namespace Subtraction
+{
+    public interface ISubtract
+    {
+        dynamic Difference(dynamic a, dynamic b);
+    }
+    public class Subtracting : ISubtract
+    {
+        private static dynamic _result;
+
+        public static dynamic Result
+        {
+            get => _result;
+            set => _result = value;
+        }
+        public static void Main()
+        {
+        }
+        public dynamic Difference(dynamic a, dynamic b)
+        {
+            Result = MathOperations.Subtraction.Difference(a, b); ;
+            return Result;
+        }
+
+    }
+}
